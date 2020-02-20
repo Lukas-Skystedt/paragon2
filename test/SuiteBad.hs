80c121e1f706d74ab89a06ed8f370578e8dc6a4e
Lukas-Skystedt <lukassk@student.chalmers.se>
2020-02-20 20:31:09 +0100

    Fixed documentation formatting in SuiteBad
    Signed-off-by: Lukas-Skystedt <lukassk@student.chalmers.se>

--- test/SuiteBad.hs
+++ test/SuiteBad.hs
@@ -1,10 +1,10 @@
 -- | Cabal test suite for tests that where errors are expected from parac. The
--- | test cases are located in the 'bad' directory. Each .para file is a test
--- | case and for each test case there is also a file containing the expected
--- | compiler output (error message).
--- |
+-- test cases are located in the 'bad' directory. Each .para file is a test
+-- case and for each test case there is also a file containing the expected
+-- compiler output (error message).
+--
 -- | The test suite is not updated for current versions of error messages and
--- | thus this suite passes the '--oldskool' flag to get the old messages.
+-- thus this suite passes the '--oldskool' flag to get the old messages.
 module SuiteBad where
 import TestUtils as TU
 import System.FilePath
@@ -12,7 +12,7 @@ import Distribution.TestSuite as TS
 import System.Console.ANSI
 
 -- | Top level function of the 'bad' test suite. Return a list of tests to be
--- | run.
+-- run.
 tests :: IO [Test]
 tests = do
   bad <- getParaFiles "test/bad"
@@ -21,7 +21,7 @@ tests = do
   return tests
 
 -- | Convert a .para file path to a Test case (assuming the path belongs to the
--- | 'bad' test suite) using the provided library.
+-- 'bad' test suite) using the provided library.
 makeTest :: FilePath -> FilePath -> Test
 makeTest lib program = Test testInst
   where testInst = TestInstance
@@ -52,7 +52,7 @@ makeTest lib program = Test testInst
             JavacErr {} -> return $ Finished $ Error "Javac should not be called for bad test cases"
 
 -- | Read the contents of file with expected test output from the file
--- | corresponding to the given .para file.
+-- corresponding to the given .para file.
 getExpected :: FilePath -> IO String
 getExpected program =
   readFile (replaceExtension program "exp")
