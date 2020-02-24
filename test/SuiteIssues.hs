d384368e19bd5ada2cdb891449b36e216bfe72ac
John Andersson <hello@johnandersson.me>
2020-02-24 12:05:57 +0100

    Fix issues test suite

--- test/SuiteIssues.hs
+++ test/SuiteIssues.hs
@@ -6,54 +6,70 @@ import System.FilePath
 import System.Directory
 import Distribution.TestSuite as TS
 import Data.List
+import Control.Monad
+import System.Console.ANSI
 
 -- | Top level function of the 'issues' test suite. Return a list of tests to be
 -- run.
 tests :: IO [Test]
 tests = do
+  removeOutput
   issues <- getAllIssues
-  print issues
-  return []
+  let tests = makeTest defaultLib <$> issues
+  return tests
 
 -- | Convert a .para file path to a Test case (assuming the path belongs to the
 -- 'issues' test suite) using the provided library.
 makeTest :: FilePath -> FilePath -> Test
-makeTest lib program = Test testInst
+makeTest lib issue = Test testInst
   where testInst = TestInstance
           { run = runTest
           -- Use the name of the .para file as test name
-          , name = takeBaseName program
+          , name = takeBaseName issue
           , tags = []
           , options = []
           , setOption = \_ _ -> Right testInst
           }
         runTest = do
-          expectedOutput <- getExpected program
-          actualOutput <- runTestCase lib program False
-          case actualOutput of
-            Success -> return $ Finished $ Fail "Parac compiled program containing errors"
-            ParacErr stdOut stdErr _exitCode
-             | (stdErr ++ stdOut) == expectedOutput -> return $ Finished Pass
-             | otherwise -> do
-                putStrLn "Exptected output:"
-                putStrLn expectedOutput
-                putStrLn "Actual output:"
-                putStrLn (stdErr ++ stdOut)
-                return $ Finished $ Fail "Expected output does not match actual output."
-            JavacErr {} -> return $ Finished $ Error "Javac should not be called for bad test cases"
+          expectedOutput <- getExpected issue
+          filesToCompile <- getIssueParaFiles issue
+          totalOut <- forM filesToCompile $ \file -> do
+            let runJavaC = null expectedOutput
+            actualOutput <- runTestCase lib file runJavaC
+            case actualOutput of
+              Success -> return ""
+              ParacErr stdOut stdErr _exitCode -> return (stdErr ++ stdOut)
+              JavacErr stdOut stdErr _exitCode -> return (stdErr ++ stdOut)
+          let concattedTotalOut = concat totalOut
+          if concattedTotalOut == expectedOutput then return $ Finished Pass else do
+            putStrLn "Exptected output:"
+            startColor Green
+            putStrLn expectedOutput
+            resetColor
+            putStrLn "Actual output:"
+            startColor Red
+            putStrLn concattedTotalOut
+            resetColor
+            return $ Finished $ Fail "Expected output doesn't match actual output"
 
 -- | Read the contents of file with expected test output from the file
 -- corresponding to the given .para file.
 getExpected :: FilePath -> IO String
-getExpected program =
-  readFile (replaceExtension program "exp")
+getExpected issue = readFile $ issue </> "expected"
+
+-- | Get paragon files to compile
+getIssueParaFiles :: FilePath -> IO [String]
+getIssueParaFiles issue = do
+  content <- readFile $ issue </> "compile"
+  pwd <- getCurrentDirectory
+  let paths = ((pwd </> issue) </>) <$> lines content
+  return paths
 
 -- | Get a list of all Issue folders.
 getAllIssues :: IO [FilePath]
 getAllIssues = do
   pwd <- getCurrentDirectory
-  print pwd
-  isList <- getDirectoryContents (pwd </> "test/issues")
-  print isList
-  let filIssues = filter ("issue" `isPrefixOf`) isList
-  return $ sort filIssues
+  isList <- listDirectory (pwd </> "test" </> "issues")
+  let filIssues = sort $ filter ("issue" `isPrefixOf`) isList
+  let paths = (("test" </> "issues") </>) <$> filIssues
+  return paths
