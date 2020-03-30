ab12de35afc8f885c61061c7a2e9d5c9b5bee40c
Lukas-Skystedt <lukassk@student.chalmers.se>
2020-03-30 15:35:00 +0200

    Work on new type checker.

--- src/Language/Java/Paragon/TypeCheck.hs
+++ src/Language/Java/Paragon/TypeCheck.hs
@@ -48,18 +48,57 @@ typeCheckCd baseName mpkg (ClassDecl sp ms i tps mSuper _impls (ClassBody _ decl
         inits       = [ idecl | idecl@InitDecl {}   <- decls ]
         supers      = maybe [objectType] (:[]) mSuper
 
-
-    return ()
+    return $ ClassBody Nothing <$> do
+      let inits' = [] -- TODO 
+          mDs' = [] -- TODO
+      -- inits' <- typeCheckInitDecls staticWPol constrWPol inits
+      -- mDs'   <- typeCheckMemberDecls staticWPol constrWPol memberDecls
+      return (inits' ++ map (MemberDecl Nothing) mDs')
 
 typeCheckCd _ _ _ = panic (typeCheckerBase ++ ".typeCheckCd")
                   "Enum decls not yet supported"
 
 typeCheckActorFields :: [MemberDecl PA] -> TcDeclM a -> TcDeclM a
 typeCheckActorFields mDecls tcba = do
+  error "typeCheckActorFields: not implemented"
     -- Find all possible actors
-    let acts = [ (ms, rt, vd)
-                     | FieldDecl _ ms (RefType _ rt) vds <- mDecls
-                     , Final defaultPos `elem` ms
-                     , vd <- vds
-                     -- Only final ones can be used in policies
-               ]
+    -- let acts = [ (ms, rt, vd)
+    --                  | FieldDecl _ ms (RefType _ rt) vds <- mDecls
+    --                  , Final defaultPos `elem` ms
+    --                  , vd <- vds
+    --                  -- Only final ones can be used in policies
+    --            ]
+
+
+typeCheckInitDecls :: ActorPolicy -> ActorPolicy -> [Decl PA] -> TcDeclM [Decl TC]
+typeCheckInitDecls sLim cLim is = error "typeCheckInitDecls: not implemented"
+
+--------------------------------------------------------------------------------
+-- Bodies
+--------------------------------------------------------------------------------
+
+typeCheckMemberDecls :: ActorPolicy
+                     -> ActorPolicy
+                     -> [MemberDecl PA] -> TcDeclM [MemberDecl TC]
+typeCheckMemberDecls sLim cLim ms = do
+  st <- setupStartState
+  mapM (typeCheckMemberDecl sLim cLim st) ms
+
+
+typeCheckMemberDecl :: ActorPolicy -> ActorPolicy -> CodeState -> TypeCheck TcDeclM MemberDecl
+typeCheckMemberDecl sLim cLim st fd@FieldDecl{} =
+    typeCheckFieldDecl sLim cLim st fd
+typeCheckMemberDecl _ _ st md@MethodDecl{} =
+    typeCheckMethodDecl st md
+typeCheckMemberDecl _ _ st cd@ConstructorDecl{} =
+    typeCheckConstrDecl st cd
+typeCheckMemberDecl _ _ _ md = return $ {-notAppl-} md
+
+typeCheckFieldDecl :: ActorPolicy -> ActorPolicy -> CodeState -> TypeCheck TcDeclM MemberDecl
+typeCheckFieldDecl staticLim constrLim st (FieldDecl _ ms _t vds) = error "typeCheckFieldDecl: not implemented"
+
+typeCheckMethodDecl :: CodeState -> TypeCheck TcDeclM MemberDecl
+typeCheckMethodDecl st (MethodDecl _ ms tps _rt i ps _exs mb) = error "typeCheckMethodDecl: not implemented"
+
+typeCheckConstrDecl :: CodeState -> TypeCheck TcDeclM MemberDecl
+typeCheckConstrDecl st (ConstructorDecl _ ms tps ci ps _exs cb) = error "typeCheckConstrDecl: not implemented"
